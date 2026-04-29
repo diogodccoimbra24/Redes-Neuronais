4d916b0996d060175dd32fb5d0e6420ba3fdacc5
diogodccoimbra24 <diogodccoimbra@gmail.com>
2026-04-29 16:28:50 +0100

    in degrre and out degree

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import networkx as nx
 import pandas as pd
 from load_graph import load_graph
 from plots import draw_graph
-from metrics import degree_calc
+from metrics import degree_calc, in_degree_calc, out_degree_calc
+
 
 #Path to the .edge file
 path = "../data/macaque.edges"
@@ -12,8 +13,10 @@ G = load_graph(path)
 #Function used to draw the graph
 draw_graph(G)
 
-#Function used calculate the degree of each node
+
 degree = degree_calc(G)
+in_degree = in_degree_calc(G)
+out_degree = out_degree_calc(G)
 
 data = []
 
@@ -21,7 +24,10 @@ data = []
 for node in nx.nodes(G):
     data.append({
         "node": node,
-        "degree": degree[node]
+        "degree": degree[node],
+        "in_degree": in_degree[node],
+        "out_degree": out_degree[node]
+
     })
 
 #Creation of the csv file with the result of the metrics
--- src/metrics.py
+++ src/metrics.py
@@ -2,11 +2,24 @@
 
 import networkx as nx
 
-
+#Fuction used to calculate the degree of each node
 def degree_calc(G):
 
     deg = dict(G.degree())
-
     return deg
+
+
+#Function used to calculate the in degree of each node
+def in_degree_calc(G):
+
+    in_deg = dict(G.in_degree())
+    return in_deg
+
+
+#Function used to calculate the out degree of each node
+def out_degree_calc(G):
+
+    out_deg = dict(G.out_degree())
+    return out_deg
 
 
